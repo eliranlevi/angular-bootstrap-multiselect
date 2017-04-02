f85abf289883163abf63a4288c19fd9817a556e4
eliranl <eliranmy8@gmail.com>
2017-04-02 23:08:22 +0300

    updates

--- src/multiselect.js
+++ src/multiselect.js
@@ -13,7 +13,17 @@
         }, object)
     };
 
-    multiselect.directive('multiselect', function ($filter, $document, $log) {
+    multiselect.getRecursiveProperty = function (object, path) {
+        return path.split('.').reduce(function (object, x) {
+            if (object) {
+                return object[x];
+            } else {
+                return null;
+            }
+        }, object);
+    };
+
+    multiselect.directive('multiselect', ['$filter', '$document', '$log', '$rootScope', function ($filter, $document, $log, $rootScope) {
         return {
             restrict: 'AE',
             scope: {
@@ -26,25 +36,25 @@
                 showUnselectAll: '=?',
                 showSearch: '=?',
                 searchFilter: '=?',
-                disabled: '=?ngDisabled',
-                defaultText: '@'
+                selectNamePl: '=',
+                selectListen: '=',
+                filterFunc: "&?",
+                disabled: '=?ngDisabled'
             },
             require: 'ngModel',
             templateUrl: 'multiselect.html',
             link: function ($scope, $element, $attrs, $ngModelCtrl) {
                 $scope.selectionLimit = $scope.selectionLimit || 0;
                 $scope.searchLimit = $scope.searchLimit || 25;
-                $scope.defaultText = $scope.defaultText || 'Select';
 
                 $scope.searchFilter = '';
 
-                $scope.resolvedOptions = [];
-                if (typeof $scope.options !== 'function') {
-                    $scope.resolvedOptions = $scope.options;
-                }
-
                 if (typeof $attrs.disabled != 'undefined') {
                     $scope.disabled = true;
+                }
+
+                if ($scope.selectListen) {
+                    $rootScope.$on(`${$scope.selectListen}.update`, handler);
                 }
 
                 $scope.toggleDropdown = function () {
@@ -66,7 +76,7 @@
                         if ($scope.selectedOptions) {
                             $scope.selectedOptions = [];
                         }
-                        $scope.unselectedOptions = $scope.resolvedOptions.slice(); // Take a copy
+                        $scope.unselectedOptions = angular.copy($scope.resolvedOptions);
                     } else {
                         $scope.selectedOptions = $scope.resolvedOptions.filter(function (el) {
                             var id = $scope.getId(el);
@@ -119,12 +129,12 @@
                         var totalSelected;
                         totalSelected = angular.isDefined($scope.selectedOptions) ? $scope.selectedOptions.length : 0;
                         if (totalSelected === 0) {
-                            return $scope.defaultText;
+                            return `כל ה${$scope.selectNamePl} נבחרו`;
                         } else {
-                            return totalSelected + ' ' + 'selected';
+                            return totalSelected + ` ${$scope.selectNamePl} ` + 'נבחרו';
                         }
                     } else {
-                        return $scope.defaultText;
+                        return `כל ה${$scope.selectNamePl} נבחרו`;
                     }
                 };
 
@@ -139,6 +149,7 @@
                 };
 
                 $scope.toggleItem = function (item) {
+                    $scope.searchFilter = '';
                     if (typeof $scope.selectedOptions === 'undefined') {
                         $scope.selectedOptions = [];
                     }
@@ -152,6 +163,8 @@
                         $scope.unselectedOptions.splice(unselectedIndex, 1);
                         $scope.selectedOptions.push(item);
                     }
+
+                    $rootScope.$broadcast(`${$attrs.ngModel}.update`);
                 };
 
                 $scope.getId = function (item) {
@@ -227,8 +240,22 @@
                     }
                 };
 
+                $scope.resolvedOptions = [];
+                if (typeof $scope.options !== 'function') {
+                    $scope.resolvedOptions = $scope.options;
+                } else {
+                    $scope.updateOptions();
+                }
+
+                function handler() {
+                    removeSelection();
+                }
+
+                function removeSelection() {
+                    $scope.selectedOptions = [];
+                }
             }
         };
-    });
+    }]);
 
 }());
